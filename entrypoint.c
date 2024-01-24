72819abca8fe972fa363356662fc71db1b72758e
GrigAnd <andrey.grigorev03@gmail.com>
2024-01-24 17:32:50 +0300

    networkfs_iterate

--- entrypoint.c
+++ entrypoint.c
@@ -8,6 +8,59 @@ MODULE_AUTHOR("GrigAnd");
 MODULE_DESCRIPTION("Hello world");
 MODULE_VERSION("1.0");
 
+#define MAX_ENTRIES 10
+
+int networkfs_iterate(struct file *filp, struct dir_context
+                                             *ctx) {
+    char fsname[10];
+    struct dentry *dentry;
+    struct inode *inode;
+    unsigned long offset;
+    int stored;
+    unsigned char ftype;
+    ino_t ino;
+    ino_t dino;
+    dentry = filp->f_path.dentry;
+    inode = dentry->d_inode;
+    offset = filp->f_pos;
+    stored = 0;
+    ino = inode->i_ino;
+
+    while (true) {
+        if (ino == 100) {
+            if (offset == 0) {
+                strcpy(fsname, ".");
+                ftype = DT_DIR;
+                dino = ino;
+            } else if (offset == 1) {
+                strcpy(fsname, "..");
+                ftype = DT_DIR;
+                dino = dentry->d_parent->d_inode->i_ino;
+            } else if (offset == 2) {
+                strcpy(fsname, "test.txt");
+                ftype = DT_REG;
+                dino = 101;
+            } else {
+                return stored;
+            }
+        }
+
+        if (dir_emit(ctx, fsname, strlen(fsname), dino, ftype)) {
+            ctx->pos += 1;
+            stored += 1;  // Increment stored if the entry is successfully emitted
+        }
+
+        offset++;
+    }
+
+return stored;
+}
+
+struct file_operations networkfs_dir_ops =
+    {
+        .iterate = networkfs_iterate,
+};
+
 struct dentry *networkfs_lookup(struct inode *parent_inode, struct dentry *child_dentry, unsigned int flag) {
     return NULL;
 }
@@ -22,6 +75,7 @@ struct inode *networkfs_get_inode(struct super_block *sb, const struct inode *di
     inode = new_inode(sb);
     inode->i_ino = i_ino;
     inode->i_op = &networkfs_inode_ops;
+    inode->i_fop = &networkfs_dir_ops;
     if (inode != NULL) {
         inode_init_owner(&init_user_ns, inode, dir, mode);
     }
@@ -30,7 +84,7 @@ struct inode *networkfs_get_inode(struct super_block *sb, const struct inode *di
 
 int networkfs_fill_super(struct super_block *sb, void *data, int silent) {
     struct inode *inode;
-    inode = networkfs_get_inode(sb, NULL, S_IFDIR | S_IRWXU | S_IRWXG | S_IRWXO, 1000);
+    inode = networkfs_get_inode(sb, NULL, S_IFDIR | S_IRWXU | S_IRWXG | S_IRWXO, 100);
     sb->s_root = d_make_root(inode);
     if (sb->s_root == NULL) {
         return -ENOMEM;
